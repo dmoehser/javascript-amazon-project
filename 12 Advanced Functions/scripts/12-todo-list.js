b7bf7854a156f3eeef48905a4f99923793d0f8e7
Daniel Möhser <danielmoehser@Mac.fritz.box>
2025-05-20 23:00:53 +0200

    Clean up directory structure: Remove duplicate folders

---
+++ 12 Advanced Functions/scripts/12-todo-list.js
@@ -0,0 +1,169 @@
+
+
+// ToDo List Practice 1
+const todoList1 = JSON.parse(localStorage.getItem('todoList1')) || [];
+
+function renderTodoList1() {
+  let todoListHtml = '';
+  
+  for (let i = 0; i < todoList1.length; i++) {
+    const todo = todoList1[i];
+    const html = `<p>${todo}</p>`;
+    todoListHtml += html;
+  }
+  
+  document.querySelector('.js-todo-list-1').innerHTML = todoListHtml;
+}
+
+function addTodo1() {
+  const inputElement = document.querySelector('.js-todo-input-1');
+  const name = inputElement.value;
+  
+  todoList1.push(name);
+  console.log('Todo Liste 1:', todoList1);
+  
+  inputElement.value = '';
+  renderTodoList1();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  renderTodoList1();
+});
+
+// ToDo List Practice 2
+const todoList2 = JSON.parse(localStorage.getItem('todoList2')) || [];
+
+function renderTodoList2() {
+  let todoListHtml = '';
+  
+  for (let i = 0; i < todoList2.length; i++) {
+    const todo = todoList2[i];
+    const html = `<p>
+      ${todo} 
+      <button onclick="
+        todoList2.splice(${i}, 1);
+        renderTodoList2();
+      ">Delete</button>
+      </p>`;
+    todoListHtml += html;
+  }
+  
+  document.querySelector('.js-todo-list-2').innerHTML = todoListHtml;
+  localStorage.setItem('todoList2', JSON.stringify(todoList2));
+}
+
+function addTodo2() {
+  const inputElement = document.querySelector('.js-todo-input-2');
+  const name = inputElement.value;
+  
+  todoList2.push(name);
+  console.log('Todo Liste 2:', todoList2);
+  
+  inputElement.value = '';
+  renderTodoList2();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  renderTodoList2();
+});
+
+// Google Gemini Output
+// ToDo List Practice 3
+const todoList3 = JSON.parse(localStorage.getItem('todoList3')) || [];
+
+function renderTodoList3() {
+  let todoListHtml = '';
+  
+  for (let i = 0; i < todoList3.length; i++) {
+    const todo = todoList3[i];
+    const html = `
+      <div class="grid-todo">
+        <div>${todo.name}</div>
+        <div>${todo.dueDate}</div>
+        <button class="delete-todo-button" data-index="${i}">Delete</button>
+      </div>
+    `;
+    todoListHtml += html;
+  }
+
+  const todoListContainer = document.querySelector('.js-todo-list-3');
+  todoListContainer.innerHTML = todoListHtml;
+  localStorage.setItem('todoList3', JSON.stringify(todoList3));
+
+  // Event-Listener für die Delete-Buttons hinzufügen (Event-Delegation)
+  todoListContainer.addEventListener('click', function(event) {
+    if (event.target.classList.contains('delete-todo-button')) {
+      const index = parseInt(event.target.dataset.index);
+      console.log('Deleted index:', index);
+      todoList3.splice(index, 1);
+      renderTodoList3();
+    }
+  });
+}
+
+// Initiales Rendern der Liste
+renderTodoList3();
+
+
+/* My ToDo List Practice 3
+// ToDo List Practice 3
+const todoList3 = JSON.parse(localStorage.getItem('todoList3')) || [];
+
+function renderTodoList3() {
+  let todoListHtml = '';
+  
+  for (let i = 0; i < todoList3.length; i++) {
+    const todo = todoList3[i];
+    const html = `
+      <div class="grid-todo">
+        <div>${todo.name}</div>
+        <div>${todo.dueDate}</div>
+        <button class="delete-todo-button" onclick="
+          todoList3.splice(${i}, 1);
+          renderTodoList3();
+        ">Delete</button>
+      </div>
+    `;
+    todoListHtml += html;
+  }
+
+
+  document.querySelector('.js-todo-list-3').innerHTML = todoListHtml;
+  localStorage.setItem('todoList3', JSON.stringify(todoList3));
+}
+  */
+
+function addTodo3() {
+  const inputElement = document.querySelector('.js-todo-input-3');
+  const dateElement = document.querySelector('.js-date-input-3');
+  const name = inputElement.value;
+  const dueDate = dateElement.value;
+  
+  if (name && dueDate) {
+    todoList3.push({
+      name: name,
+      dueDate: dueDate
+    });
+    
+    console.log('Todo Liste 3:', todoList3);
+    
+    inputElement.value = '';
+    dateElement.value = '';
+    renderTodoList3();
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  renderTodoList3();
+});
+
+
+// Event Listener for the "Add Todo"-Buttons
+document.querySelector('.js-add-todo-button1')
+  .addEventListener('click', addTodo1);
+
+  document.querySelector('.js-add-todo-button2')
+  .addEventListener('click', addTodo2);
+
+  document.querySelector('.js-add-todo-button3')
+  .addEventListener('click', addTodo3);
--- 13-javascript-amazon-project/scripts/checkout.js
+++
@@ -1,7 +0,0 @@
-import { cart, calculateCartQuantity } from './data/cart.js';
-import { deliveryOptions } from './scripts/deliveryOptions.js';
-import { formatCurrency } from './utilis/money.js';
-import { renderPaymantSummary } from './checkout/paymantSummary.js';
-import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-
-renderPaymantSummary();
